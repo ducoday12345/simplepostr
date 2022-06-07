eb7793ee69013b7ca3cb3c9dc0e5a794f4a9c43f
ducoday12345 <104491261+ducoday12345@users.noreply.github.com>
2022-06-06 23:41:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from'./routes/posts.js';
+import userRoutes from './routes/users.js'
+
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb" , extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
+app.use('/posts', postRoutes);
+app.use('/user',userRoutes);
+
+app.get('/', ( req,res)=>{
+    res.send('APP IS RUNNING. ');
+})
+
+const CONNECTION_URL = 'mongodb+srv://ducoday12345:duc200601@cluster0.k73zc.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
+//https://www.mongodb.com/atlas/database
